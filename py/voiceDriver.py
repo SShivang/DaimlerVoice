1481f2950c216563f02dd95e8ec22822da992b2a
Shivang SIngh <ShivangWork@wireless-10-146-203-99.public.utexas.edu>
2019-07-20 21:34:02 -0500

    diagnostics

--- py/voiceDriver.py
+++ py/voiceDriver.py
@@ -47,17 +47,14 @@ def get_intent(sio):
                 command = transcript['transcript']
                 print('command is: ')
                 print(command)
-                response = detect_intent_texts("daimlervoice-xadvoe", "AIzaSyAC8ja1pF9UmPId7MUZhbB8hAY8P_HWW7E", command , "en")                    
+                response = detect_intent_texts("daimlervoice-xadvoe", "AIzaSyAC8ja1pF9UmPId7MUZhbB8hAY8P_HWW7E", command , "en")
     else:
         print('enter next command')
         command = raw_input()
         response = detect_intent_texts("daimlervoice-xadvoe", "AIzaSyAC8ja1pF9UmPId7MUZhbB8hAY8P_HWW7E", command , "en")
     print(response)
-    
+
     return response.query_result if response != 'empty response!' else None
-
-
-
 
 
 def handle_global_intent(sio, response):
@@ -67,72 +64,132 @@ def handle_global_intent(sio, response):
         print('No globals')
 
 
-def handle_step3(sio, cur_state):
-    print('Check the gasoline')
-    return {
-        'next_handler': main_menu,
-        'done': False
-    }
+# def handle_step3(sio, cur_state):
+#     print('Check the gasoline')
+#     return {
+#         'next_handler': main_menu,
+#         'done': False
+#     }
+#
+# def handle_step2(sio, cur_state):
+#     print('Check the gasoline')
+#     return {
+#         'next_handler': main_menu,
+#         'done': False
+#     }
+#
+#
+#
+# def handle_step1(sio, cur_state):
+#     print('Is the truck on?')
+#     while True:
+#         print('beginning step 1')
+#
+#         response = get_intent(sio)
+#         if not response: continue
+#         if(response.intent.display_name in GLOBAL_INTENTS):
+#             handle_global_intent(sio, response)
+#         elif response.intent.display_name == YES_INTENT:
+#             print('exit step 1')
+#             # add_to_db(step1info)
+#             return {
+#                 'next_handler' : handle_step2,
+#                 'done' : cur_state['done']
+#             }
+#
+#         elif response.intent.display_name == NO_INTENT:
+#             return {
+#                 'next_handler' : handle_step3,
+#                 'done' : False
+#             }
+#         else:
+#             print('input not accepted')
+#
 
+# def main_menu(sio, cur_state):
+#     print('Welcome to the diagnostic tool. What would you like to do?')
+#     while True:
+#         response = get_intent(sio)
+#         if not response: continue
+#         if(response.intent.display_name in GLOBAL_INTENTS):
+#             handle_global_intent(sio, response)
+#         elif response.intent.display_name == RUN_DIAGNOSTICS:
+#             # add_to_db(step1info)
+#             return {
+#                 'next_handler' : handle_step1,
+#                 'done' : cur_state['done']
+#             }
+#         else:
+#             print('input not accepted')
 
+smartList = [
+{
+    'Step' : 0,
+    'Text' : "NULL",
+    'YES'  : 0,
+    'NO'   : 0,
+    'Value-Required' : 'YES'
+ },
 
-def handle_step2(sio, cur_state):
-    print('Check the gasoline')
-    return {
-        'next_handler': main_menu,
-        'done': False
-    }
+{
+    'Step' : 1,
+    'Text' : "Yeet",
+    'YES'  : 4,
+    'NO'   : 2,
+    'Value-Required' : 'YES'
 
+},
 
+{
+    'Step' : 2,
+    'Text' : "Yeeter",
+    'YES'  : 0,
+    'NO'   : 3,
+    'Value-Required' : 'YES'
+},
 
-def handle_step1(sio, cur_state):
-    print('Is the truck on?')
-    while True:
-        print('beginning step 1')
+{
+    'Step' : 3,
+    'Text' : "Whear",
+    'YES'  : 4,
+    'NO'   : 0,
+    'Value-Required' : 'YES'
 
+},
+
+{
+    'Step' : 4,
+    'Text' : "My name is sam",
+    'YES'  : 0,
+    'NO'   : 0,
+    'Value-Required' : 'YES'
+
+}]
+
+def getParts(sio, part_str):
+    print("Looking for" + part_str)
+
+    #Check how many of a certain part is avaliable
+
+def contactCustomer(sio):
+    print("Calling customer")
+    #Need to check the last diagnostic this person ran
+
+    #Twilio
+
+def runDiagnostics(sio):
+    print('Running some diagnostics')
+    state = smartList[1]
+    while state['Step'] != 0:
+        print(state['Value-Required'])
         response = get_intent(sio)
-        if not response: continue
         if(response.intent.display_name in GLOBAL_INTENTS):
             handle_global_intent(sio, response)
-        elif response.intent.display_name == YES_INTENT:
-            print('exit step 1')
-            # add_to_db(step1info)
-            return {
-                'next_handler' : handle_step2,
-                'done' : cur_state['done']
-            }
-
-        elif response.intent.display_name == NO_INTENT:
-            return {
-                'next_handler' : handle_step3,
-                'done' : False
-            }
-        else:
-            print('input not accepted')
-        
-
-def main_menu(sio, cur_state):
-    print('Welcome to the diagnostic tool. What would you like to do?')
-    while True:
-        response = get_intent(sio)
-        if not response: continue
-        if(response.intent.display_name in GLOBAL_INTENTS):
-            handle_global_intent(sio, response)
-        elif response.intent.display_name == RUN_DIAGNOSTICS:
-            # add_to_db(step1info)
-            return {
-                'next_handler' : handle_step1,
-                'done' : cur_state['done']
-            }
-        else:
-            print('input not accepted')
-
-
-
-state = {
-    'next_handler' : main_menu,
-    'done': False
-}
+        if (response.intent.display_name == YES_INTENT):
+            state = smartList[state['YES']]
+        if (response.intent.display_name == NO_INTENT):
+            state = smartList[state['NO']]
+    print('diagnostics are over')
 
 def runDriver(sio):
     global mic
@@ -141,8 +198,16 @@ def runDriver(sio):
     textInput = raw_input()
     if textInput == 'y':
         mic = True
-    while not state['done']:
-        state = state['next_handler'](sio, state)
+
+    #if any part is not in stock ask if you want to order it
+    
+    while True:
+        temp = get_intent(textInput)
+        state = {
+            'done' : False
+        }
+        if (temp.intent.display_name == RUN_DIAGNOSTICS):
+            runDiagnostics(sio)
 
     sio.disconnect()
 
@@ -171,13 +236,10 @@ def runDriver(sio):
 #                     response = detect_intent_texts("daimlervoice-xadvoe", "AIzaSyAC8ja1pF9UmPId7MUZhbB8hAY8P_HWW7E", command , "en")
 #                     web_text = response.query_result.parameters.fields['displayText'].string_value
 #                     sio.emit('voice', { 'data': web_text })
-                    
+
 #             else:
 #                 print('enter next command')
 #                 command = raw_input()
 #                 response = detect_intent_texts("daimlervoice-xadvoe", "AIzaSyAC8ja1pF9UmPId7MUZhbB8hAY8P_HWW7E", command , "en")
 #                 web_text = response.query_result.parameters.fields['displayText'].string_value
 #                 sio.emit('voice', { 'data': web_text })
-
-
-        
